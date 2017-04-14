fc44e2cf3dcc4903e005dec1c09cfef34317fd4c
KaiZhu <1564861584@qq.com>
2017-04-14 14:03:51 +0800

    标定代码

--- R360_StructureLight/inc/Calibrate.h
+++ R360_StructureLight/inc/Calibrate.h
@@ -7,6 +7,15 @@
 #ifndef CALIBRATE_ZK_H
 #define CALIBRATE_ZK_H
 
-#include <string>
+#include <highgui.h> 
+#include "cv.h"
+#include <iostream> 
+#include "opencv2/opencv.hpp"
+
+extern std::vector<CvMat> T_mat_4x4;					//Ðý×ª¾ØÕó
+
+void inputCameraParam(CvMat * intrinsic_matrix1, CvMat * distortion_coeffs1);
+void caculate_Tmat(CvMat * r_vec, CvMat * t_vec, CvMat * T_mat);
+int find_rotation_mat();
 
 #endif
